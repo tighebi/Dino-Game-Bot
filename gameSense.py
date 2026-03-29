9a3bd5816d8dc1588d6394632f92ac3de44769c3
Tighe B <tigheb@bu.edu>
2026-03-29 13:27:57 -0400

    Massively increased frame rate so it is more consistent

--- gameSense.py
+++ gameSense.py
@@ -1,23 +1,27 @@
 import pyautogui
 import time
+import mss
+import numpy as np
+ 
 
 pyautogui.time.sleep(3)
 
 frame = 0
+c = 10
 last_time = time.time()
 
-while "Dino running":
-    frame += 1
-    x, y = pyautogui.position()
-    rgb = pyautogui.pixel(x,y)
+with mss.mss() as sct:
+    while "Dino running":
+        frame += 1
+        x, y = pyautogui.position()
 
-    brightness = ( rgb.red + rgb.green + rgb.blue ) /3
+        monitor = {"top": y-c, "left": x-c, "width": 2*c, "height": 2*c}
+        img = np.array(sct.grab(monitor))
 
-    if brightness < 128:
-        pyautogui.hotkey("space")
-    
-    print("Run or jump")
-    if frame % 200 == 0:
-        print(f"fps: {frame/(time.time()-last_time)}")
-        last_time = time.time()
-        frame = 0
+        if (img[:,:,:3].mean(axis=2) < 128).any():
+            pyautogui.hotkey("space")
+        
+        if frame == 500:
+            print(f"fps: {frame/(time.time()-last_time)}")
+            last_time = time.time()
+            frame = 0            
